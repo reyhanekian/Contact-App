6a495015c4d233dbd423db0c42af80a60fd52e39
reyhan <reyanekia@gmail.com>
2024-11-12 09:39:25 -0800

    Create Contacts App

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+import Contacts from './components/Contacts'
+  
+
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <Contacts/>
+    </>
+  )
+}
